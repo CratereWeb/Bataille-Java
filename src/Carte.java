60154e1135e70b755ab779de15884cc098a9ee52
Mac <jackocratere@gmail.com>
2022-09-08 18:45:25 +0200

    first commit

---
+++ src/Carte.java
@@ -0,0 +1,9 @@
+
+public class Carte {
+	// Contient un constructeur pour créer une carte
+	
+	// Attributs: couleur, valeur
+		// parmi un tableau
+	// Méthodes: accesseurs, modificateurs, comparateur
+	
+}
---
+++ src/Joueur.java
@@ -0,0 +1,13 @@
+
+public class Joueur {
+	/*
+	 *  Propriétés :
+	 *		- cartes dans son jeu (tableau)
+	 *		- compteur de points
+	 *	Méthodes :
+	 *		- tirer une carte de son paquet
+	 *		- poser la carte et la jouer
+	 *		
+	 */
+	
+}
